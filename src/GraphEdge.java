87f631f6421f4db395e58d4d57eadad4b78af679
arbel <arbelvsr@gmail.com>
2021-12-14 17:32:13 +0200

    init commit

--- src/GraphEdge.java
+++ src/GraphEdge.java
@@ -1,2 +1,9 @@
 public class GraphEdge {
+    GraphNode from;
+    GraphNode to;
+
+    public GraphEdge(GraphNode f, GraphNode t){
+        from=f;
+        to=t;
+    }
 }
--- src/RootedTree.java
+++ src/RootedTree.java
@@ -1,5 +1,17 @@
 import java.io.DataOutputStream;
 import java.io.IOException;
 public class RootedTree {
+    GraphNode root;
+
+
+    public RootedTree(){
+        root=null;
+    }
+    public void printByLayer(DataOutputStream out) throws IOException {
+        out.write(root.key);
+        //use bfs
+
+
+    }
 
 }
